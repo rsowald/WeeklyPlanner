4dde6df3aa919ae1f21adc5918353e99afa8d05b
rsowald <72467920+rsowald@users.noreply.github.com>
2021-01-24 15:48:22 -0800

    removed 1800 time block and applied color coding to time blocks

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,23 @@
+//Grab current day and date from dayjs and put in header
 $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"))
-var currentTime = dayjs().format("H:m:s")
-console.log(currentTime)
 
+//If dayjs().format(HH:mm)===23:59, clear local storage?
+
+
+var currentHour = dayjs().hour()
+console.log(currentHour)
+
+$('.row textarea').each(function (i) {
+    var hour = i + 9; //Our page's requirement is to show the workday, 9am - 5pm, so we offset our loop by 9 hours
+
+    if (hour < currentHour) {
+        $(this).addClass('past');
+    }
+    else if (hour === currentHour) {
+        $(this).addClass('present');
+    }
+    else {
+        $(this).addClass('future');
+    }
+});
 
