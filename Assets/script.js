6972b9a228efe024c76006839f86a0db479ccfbe
rsowald <72467920+rsowald@users.noreply.github.com>
2021-01-26 17:06:54 -0800

    refactor pulling from local storage

--- Assets/script.js
+++ Assets/script.js
@@ -24,68 +24,21 @@ $(document).ready(function () {
         }
     });
 
-    function initSchedule() {
-        var saved9 = JSON.parse(localStorage.getItem('hour9'));
-        console.log(saved9)
-        if (saved9 !== null) {
-            $("#hour9").text(saved9);
-        }
-        var saved10 = JSON.parse(localStorage.getItem('hour10'));
-        console.log(saved10)
-        if (saved10 !== null) {
-            $("#hour10").text(saved10);
-        }
-        var saved11 = JSON.parse(localStorage.getItem('hour11'));
-        console.log(saved11)
-        if (saved11 !== null) {
-            $("#hour11").text(saved11);
-        }
-        var saved12 = JSON.parse(localStorage.getItem('hour12'));
-        console.log(saved12)
-        if (saved12 !== null) {
-            $("#hour12").text(saved12);
-        }
-        var saved13 = JSON.parse(localStorage.getItem('hour13'));
-        console.log(saved13)
-        if (saved13 !== null) {
-            $("#hour13").text(saved13);
-        }
-        var saved14 = JSON.parse(localStorage.getItem('hour14'));
-        console.log(saved14)
-        if (saved14 !== null) {
-            $("#hour14").text(saved14);
-        }
-        var saved15 = JSON.parse(localStorage.getItem('hour15'));
-        console.log(saved15)
-        if (saved15 !== null) {
-            $("#hour15").text(saved15);
-        }
-        var saved16 = JSON.parse(localStorage.getItem('hour16'));
-        console.log(saved16)
-        if (saved16 !== null) {
-            $("#hour16").text(saved16);
-        }
-        var saved17 = JSON.parse(localStorage.getItem('hour17'));
-        console.log(saved17)
-        if (saved17 !== null) {
-            $("#hour17").text(saved17);
-        }
+    for (var i = 9; i < 18; i++) {
+        $('#hour' + i).text(JSON.parse(localStorage.getItem('hour' + i)));
     }
 
-    function saveHour() {
-        // Attach "ON CLICK" events to the save buttons
-        $(".saveBtn").on("click", function (event) {
-            event.preventDefault();
+    // Attach "ON CLICK" event to all the save buttons
+    $(".saveBtn").on("click", function (event) {
+        //prevent default so I can apply a for loop to all the time blocks
+        event.preventDefault();
 
-            // Iterate through hours 9 to 17
-            for (var i = 9; i < 18; i++) {
-                localStorage.setItem("hour" + i, JSON.stringify($("#hour" + i).val()));
-                console.log("hour" + i);
-            }
-        });
-    }
+        // Iterate through hours 9 to 17
+        for (var i = 9; i < 18; i++) {
+            localStorage.setItem("hour" + i, JSON.stringify($("#hour" + i).val()));
+            console.log("hour" + i);
+        }
+    });
 
-    initSchedule()
-    saveHour()
 
 });
